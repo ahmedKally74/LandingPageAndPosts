5d307c9ac8be0b9fda351f994f5506ed9233db80
Ahmed <ahmed.acu3@gmail.com>
2024-09-28 09:29:31 +0300

    LandingPage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import '../src/styles/global.css'
-import LandingPage from './components/LandingPage';
+import "../src/styles/global.css";
+import LandingPage from "./components/LandingPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Posts from './components/Posts/Posts';
-
+import Posts from "./components/Posts/Posts";
 
 function App() {
   return (
     <>
-   
       <BrowserRouter>
-     <Routes>
-        <Route exact  path="/" element={    <LandingPage />} />
-        <Route exact  path="/posts" element={ <Posts />} />
-    </Routes>
-    </BrowserRouter>
-      
-         {/* <UserList /> */}
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/posts" element={<Posts />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
